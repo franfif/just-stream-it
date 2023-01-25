6516ca724423fa851e8f98d647e6472a30b8cf04
Francis Barrow <francis.barrow@gmail.com>
2023-01-25 15:59:52 -0500

    clean up modal and slider

--- src/modal.js
+++ src/modal.js
@@ -1,50 +1,34 @@
-const modalCloser = document.querySelector(".modal__close-btn");
-const modalList = document.querySelector(".modal__list");
-const modalMovieDetails = document.getElementById("myModal");
-
 /* Modal */
-// function modalUp() {
-//     console.log('call ModalUp');
-//     const modalOpeners = document.querySelectorAll(".modal-movie-details--opener");
-//     console.log('modalOpeners');
-//     console.log(modalOpeners);
+const MODAL_CLOSER = document.querySelector(".modal__close-btn");
+const MODAL_LIST = document.querySelector(".modal__list");
+const MODAL_ELEMENT = document.getElementById("modal-movie-details");
 
-//     // Get the movieId attribute value from the closest ancestor with a movie-id class
-//     function getMovieId(element) {
-//         return element.closest(".movie-id").getAttribute('movieId');
-//     }
-//     // Create eventlistener for each element able to open a modal
-//     modalOpeners.forEach(opener => {
-//         opener.addEventListener('click', (event) => {
-//             fillModal(getMovieId(event.target));
-//             modalMovieDetails.style.display = "block";
-//             document.body.classList.add("no-scroll");
-//         })
-//     })
-// }
-
+// Open modal
 function openingModal(movieId) {
+    // Add information to modal
     fillModal(movieId);
-    modalMovieDetails.style.display = "block";
+    // Diplay modal element
+    MODAL_ELEMENT.style.display = "block";
+    // Block scrolling of the page
     document.body.classList.add("no-scroll");
 }
 
 // Manage the closing of a modal
 function closingModal() {
     // Hide the modal
-    modalMovieDetails.style.display = "none";
+    MODAL_ELEMENT.style.display = "none";
     // Empty the content of the modal
-    modalList.innerHTML = "";
+    MODAL_LIST.innerHTML = "";
     // Allow back scrolling on the main page
     document.body.classList.remove("no-scroll");
 }
 
 // Create eventlistener for the closing button in the modal
-modalCloser.addEventListener('click', closingModal)
+MODAL_CLOSER.addEventListener('click', closingModal)
 
 // Close the modal if user click outside of it
 window.addEventListener('click', event => {
-    if (event.target == modalMovieDetails) {
+    if (event.target == MODAL_ELEMENT) {
         closingModal();
     }
 })
