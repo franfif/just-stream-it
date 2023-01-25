c12274d4dc5a897bf92153830005a985774f3290
Francis Barrow <francis.barrow@gmail.com>
2023-01-25 12:46:49 -0500

    replace slider and arrow handling

--- src/slider.js
+++ src/slider.js
@@ -1,29 +1,42 @@
 /* Slider */
-// Set all sliders scroll to the left
-const sliders = document.querySelectorAll(".slider");
-sliders.forEach(slider => {
-    slider.scrollLeft = 0;
-});
+function setSliders() {
+    console.log('call setSliders()');
+    // Set all sliders scroll to the left
+    const sliders = document.querySelectorAll(".slider");
+    for (const slider of sliders) { slider.scrollLeft = 0 }
 
-// Get all arrows
-const leftArrows = document.querySelectorAll(".arrow-holder__left");
-const rightArrows = document.querySelectorAll(".arrow-holder__right");
 
-// Add eventlistener to arrows to activate horizontal scroll
-leftArrows.forEach(arrow => {
-    arrow.addEventListener('click', (event) => scrollHorizontal(event, toTheLeft = true));
-})
-rightArrows.forEach(arrow => {
-    arrow.addEventListener('click', (event) => scrollHorizontal(event, toTheLeft = false));
-})
+    // Add functionality to all arrows
+    // Get all arrows
+    const leftArrows = document.querySelectorAll(".arrow-holder__left");
+    const rightArrows = document.querySelectorAll(".arrow-holder__right");
 
-// Define horizontal scroll
-function scrollHorizontal(event, toTheLeft = false) {
-    // Make sure any cut thumbnail will be shown after scroll
+    // Add eventlistener to arrows to activate horizontal scroll
+    leftArrows.forEach(arrow => {
+        arrow.addEventListener('click', (event) => scrollHorizontal(event, toTheLeft = true));
+    })
+    rightArrows.forEach(arrow => {
+        arrow.addEventListener('click', (event) => scrollHorizontal(event, toTheLeft = false));
+    })
+
+    // Define horizontal scroll
+    function scrollHorizontal(event, toTheLeft = false) {
+        // Make sure any cut thumbnail will be shown after scroll
+        let scrollValue = visualViewport.width - 180; //180 = max-width of figure caption.
+        scrollValue = toTheLeft ? -scrollValue : scrollValue;
+        // Get slider element from the category
+        const slider = event.target.closest(".slider__container").querySelector(".slider");
+        slider.scroll({
+            left: slider.scrollLeft + scrollValue,
+            behavior: 'smooth'
+        });
+    }
+}
+function scrollHorizontally(arrow, toTheLeft = false) {
     let scrollValue = visualViewport.width - 180; //180 = max-width of figure caption.
     scrollValue = toTheLeft ? -scrollValue : scrollValue;
     // Get slider element from the category
-    const slider = event.target.closest(".slider__container").querySelector(".slider");
+    const slider = arrow.closest(".slider__container").querySelector(".slider");
     slider.scroll({
         left: slider.scrollLeft + scrollValue,
         behavior: 'smooth'
