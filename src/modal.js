91b4e6dc28f6e734896679b8ae9ff3d26129b855
Francis Barrow <francis.barrow@gmail.com>
2023-01-26 11:19:31 -0500

    clean up code

--- src/modal.js
+++ src/modal.js
@@ -11,7 +11,7 @@ function openingModal(movieId) {
     MODAL_ELEMENT.style.display = "block";
     // Block scrolling of the page
     document.body.classList.add("no-scroll");
-}
+};
 
 // Manage the closing of a modal
 function closingModal() {
@@ -21,14 +21,14 @@ function closingModal() {
     MODAL_LIST.innerHTML = "";
     // Allow back scrolling on the main page
     document.body.classList.remove("no-scroll");
-}
+};
 
 // Create eventlistener for the closing button in the modal
-MODAL_CLOSER.addEventListener('click', closingModal)
+MODAL_CLOSER.addEventListener('click', closingModal);
 
 // Close the modal if user click outside of it
 window.addEventListener('click', event => {
     if (event.target == MODAL_ELEMENT) {
         closingModal();
     }
-})
+});
