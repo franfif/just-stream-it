cd387049ede3628b81b4c1bc8c5ed00c77125240
Francis Barrow <francis.barrow@gmail.com>
2023-01-26 11:28:19 -0500

    update img folder

--- src/image.js
+++ src/image.js
@@ -1,6 +1,6 @@
 // Replace any image not loaded by a dummy image
 function useDummyImage(image) {
     image.onerror = "";
-    image.src = "./public/image/No_movie_image.png";
+    image.src = "./img/No_movie_image.png";
     return true;
 };
